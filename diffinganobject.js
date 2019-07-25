5441f99b32819d143c96e33e9b18419195e3e37c
smv5047 <smv5047@gmail.com>
2019-07-25 08:33:42 -0400

    Completed JS 1 challange

--- diffinganobject.js
+++ diffinganobject.js
@@ -7,26 +7,42 @@
 
 //Given JavaScript objects like this:
 
-```newCode = {
-   apples: 3, 
-   oranges: 4
-}```
+// ```newCode = {
+//    apples: 3, 
+//    oranges: 4
+// }```
 
-```oldCode = {
-   apples: 3, 
-   grapes: 5
-}```
+// ```oldCode = {
+//    apples: 3, 
+//    grapes: 5
+// }```
 
 //Create a function that returns an array of containing the object diff like the:
 
-```diff(newCode, oldCode)```
+// ```diff(newCode, oldCode)```
 
-```//returns: 
-// [
-//     ['-', 'grapes', 5],
-//     ['+', 'oranges',4]
-// ]```
+// ```//returns: 
+// // [
+// //     ['-', 'grapes', 5],
+// //     ['+', 'oranges',4]
+// // ]```
 
 //When you have figured this out, DM your TL the solution that you have come up with.  
 //It is also highly recommended that you create a repository on Github named coding challenges 
 //and upload your work to Github.  That way you keep getting green ticks on your Github graph.
+
+
+//Compare 2 objects and determine the difference
+//return the difference
+function diff (newCode, oldCode) {
+    let difference = {};
+    for (let i =0; i<newCode.length; i++) {
+        for (let y=0; y<oldCode.length; y++) {
+            if (newCode.i === oldCode.y){
+                difference.push(new)
+            }
+        }
+    }
+}
+
+
