f82afb459729a4c6164acab03db6c527acd5e26d
smv5047 <smv5047@gmail.com>
2019-08-19 22:08:25 -0400

    Added comments to vowel counter

---
+++ vowelCounter.js
@@ -0,0 +1,17 @@
+//Write a function which counts the number of vowels in a given string. Return the count number.
+function vowelCount(str) {
+    let newArray = []
+    let newString = str.toLowerCase()
+    newArray = newString.split("")
+    let vowelNum = 0
+    for (let i=0; i<newArray.length; i++) {
+      if (newArray[i] ==  "a" || newArray[i] ==  "e" || newArray[i] ==  "i" || newArray[i] ==  "o" || newArray[i] ==  "u") {
+        vowelNum = vowelNum+1
+      }
+    }
+    
+    return vowelNum
+  }
+  
+  
+  vowelCount("Hello World")
