ab8bef26fea793c45050f61f75918d4e7213652f
smv5047 <smv5047@gmail.com>
2019-11-10 18:44:40 -0500

    To Binary String Completed

--- ToBinaryString.js
+++ ToBinaryString.js
@@ -7,26 +7,40 @@
 
 function toBinaryString(number) {
  
-    let binaryString = "0"
-   
-  
-    if (number>1) {
-      let x = number%2
-      binaryString =  x + binaryString
-    } 
-    if (number>1) {
-      let x = number%2
-      binaryString =  x + binaryString
-    } 
-  
-    
-    return binaryString
+  let binaryString = ""
+
+  if(number === 0) {
+    return "0"
   }
   
-  
-  
-  
-  
-  console.log(toBinaryString(5));  // <--- 101
+  while (number>0) {
+    let x = number%2
+    binaryString =  x + binaryString
+    number = Math.floor(number/2)
+  }
 
-  ///Updated needed
+  
+  return binaryString
+}
+
+
+
+
+
+console.log(toBinaryString(0));  // <--- 0
+console.log(toBinaryString(1));  // <--- 1
+console.log(toBinaryString(2));  // <--- 10
+console.log(toBinaryString(3));  // <--- 11
+console.log(toBinaryString(4));  // <--- 100
+console.log(toBinaryString(5));  // <--- 101
+console.log(toBinaryString(6));  // <--- 110
+console.log(toBinaryString(7));  // <--- 111
+console.log(toBinaryString(8));  // <--- 1000
+console.log(toBinaryString(9));  // <--- 1001
+console.log(toBinaryString(10)); // <--- 1010
+console.log(toBinaryString(11)); // <--- 1011
+console.log(toBinaryString(12)); // <--- 1100
+console.log(toBinaryString(13)); // <--- 1101
+console.log(toBinaryString(14)); // <--- 1110
+console.log(toBinaryString(15)); // <--- 1111
+console.log(toBinaryString(16)); // <--- 10000
