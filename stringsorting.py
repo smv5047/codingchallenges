615c4aa03b854b720228324e4756a451f9fe025a
smv5047 <smv5047@gmail.com>
2020-08-05 19:52:52 -0400

    string sorting challenge

---
+++ stringsorting.py
@@ -0,0 +1,33 @@
+# Print out all of the strings in the following array in alphabetical order, each on a separate line.
+# ['Waltz', 'Tango', 'Viennese Waltz', 'Foxtrot', 'Cha Cha', 'Samba', 'Rumba', 'Paso Doble', 'Jive']
+# The expected output is:
+
+# 'Cha Cha'
+# 'Foxtrot'
+# 'Jive'
+# 'Paso Doble'
+# 'Rumba'
+# 'Samba'
+# 'Tango'
+# 'Viennese Waltz'
+# 'Waltz'
+
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+test_array = ['Waltz', 'Tango', 'Viennese Waltz', 'Foxtrot',
+              'Cha Cha', 'Samba', 'Rumba', 'Paso Doble', 'Jive']
+
+
+def alphapetical_sort(array):
+
+    array.sort()
+    for word in array:
+        print(word)
+
+
+alphapetical_sort(test_array)
+
+
+def middle_sort(array):
+    # return middle character
