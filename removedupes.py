5d68e7ead5af58124759cc3b2ae9cd208d5e59d4
smv5047 <smv5047@gmail.com>
2020-09-20 17:38:54 -0400

    dried up solution

--- removedupes.py
+++ removedupes.py
@@ -1,15 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums):
         # iterate over array
-
         # if value of i is equal to i+1, move i+1 to the end of the array
         # when i <= i+1 break and return i
-        # while (nums[i]<=nums[i+1]):
-        #     if (nums[i] == nums[i+1]):
-        #         nums[-1] = nums[i+1]
-        #         i+=2
-        #     else:
-        #         i+=1
+
         for i, num in enumerate(nums):
             if i < (len(nums)-1):
                 if (nums[i] > nums[i+1]):
